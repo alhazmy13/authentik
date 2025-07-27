2ccf81ca6b81cc40f1c796cc713568752f5a3ba1
Abdullah Alhazmy <me@alhazmy13.net>
2025-07-27 11:18:50 +0300

    update ci

--- .github/actions/docker-push-variables/test.sh
+++ .github/actions/docker-push-variables/test.sh
@@ -4,15 +4,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 GITHUB_OUTPUT=/dev/stdout \
     GITHUB_REF=ref \
     GITHUB_SHA=sha \
-    IMAGE_NAME=ghcr.io/goauthentik/server,beryju/authentik \
-    GITHUB_REPOSITORY=goauthentik/authentik \
+    IMAGE_NAME=ghcr.io/alhazmy13/server,beryju/authentik \
+    GITHUB_REPOSITORY=alhazmy13/authentik \
     python $SCRIPT_DIR/push_vars.py
 
 # Pushing PR/main
 GITHUB_OUTPUT=/dev/stdout \
     GITHUB_REF=ref \
     GITHUB_SHA=sha \
-    IMAGE_NAME=ghcr.io/goauthentik/server,beryju/authentik \
-    GITHUB_REPOSITORY=goauthentik/authentik \
+    IMAGE_NAME=ghcr.io/alhazmy13/server,beryju/authentik \
+    GITHUB_REPOSITORY=alhazmy13/authentik \
     DOCKER_USERNAME=foo \
     python $SCRIPT_DIR/push_vars.py
